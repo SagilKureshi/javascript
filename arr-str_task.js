c9109a796f9e978e51358edb9bc95087487479a6
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-05-05 17:30:38 +0530

    Comment out code for task organization
    Comment out existing code for tasks 1, 2, 3, and 4.

--- arr-str_task.js
+++ arr-str_task.js
@@ -1,7 +1,33 @@
-let arr = [55,55,55,55,55];
-console.log(arr);
-let sum = arr.reduce((acc,current)=>{
-	return (acc + current);
-},0);
-let avg = sum/arr.length;
-console.log("Average marks = ", avg);
+//Task 1
+//let arr = [55,55,55,55,55];
+//console.log(arr);
+//let sum = arr.reduce((acc,current)=>{
+//	return (acc + current);
+//},0);
+//let avg = sum/arr.length;
+//console.log("Average marks = ", avg);
+
+//Task 2
+
+//let n = 6;
+//let arr = new Array(n).fill(0);
+//console.log(arr);
+
+// Task 3 method 1
+
+//let n = 7;
+//let arr = [];
+//for(let s = 1; s <= n; s++){
+//	arr[s] = s;
+//}	
+//console.log(arr);
+
+//Task 4 method 2
+
+/*let n = 5;
+let arr = new Array(n).fill(0);
+arr.forEach((item,index)=>{
+	arr[index] = index+1;
+});
+console.log(arr);*/
+
