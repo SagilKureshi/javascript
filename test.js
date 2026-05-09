1920d900233902fe3a0c93b4739a1fb2d7af7fbf
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-05-09 17:47:59 +0530

    Add files via upload

--- test.js
+++ test.js
@@ -1,1 +1,33 @@
-console.log("hello");
+// // // // let div = document.querySelector('div');
+// // // // console.log(div);
+// // // // let p = document.createElement('p');
+// // // // console.log(p);
+// // // // div.beforee (p);
+// // // // p.innerText = "jjjj";
+// // // let p = document.querySelector('p');
+// // // //p.setAttribute('class','bgcolor');
+// // // p.style.backgroundColor = "yellow";
+
+// // let div = document.querySelector('.div1');
+// // div.addEventListener('touchmove',()=>{
+// //     console.log("Touch moving");
+// // })
+
+// let p = document.querySelector('#p1');
+// p.addEventListener('copy',()=>{
+//     console.log(window.getSelection()+" is coping");
+// })
+
+//counter value increment & decrement task
+
+let minus = document.querySelector('#minus');
+let pluse = document.querySelector('#pluse');
+let value = document.querySelector('.value');
+
+minus.addEventListener('click',()=>{
+    value.innerHTML = Number(value.innerHTML )-1;
+});
+
+pluse.addEventListener('click',()=>{
+    value.innerHTML = Number(value.innerHTML )+1;
+});
---
+++ theme_change.js
@@ -0,0 +1,56 @@
+// // // // // let button = document.querySelector('button');
+// // // // // let body = document.querySelector('body');
+
+// // // // // let theme = 'light';
+// // // // // button.addEventListener('click',()=>{
+// // // // //     if(theme === 'light'){
+
+// // // // //         body.style.backgroundColor = 'black';
+// // // // //         body.style.color = 'white';
+// // // // //         theme = 'black';
+// // // // //     }
+// // // // //     else{
+// // // // //         body.style.backgroundColor = 'white';
+// // // // //         body.style.color = 'black';
+// // // // //         theme = 'light'
+// // // // //     }
+// // // // // });
+// // // // // let button = document.querySelector('button');
+// // // // // let body = document.querySelector('body');
+
+// // // // // button.addEventListener('click',()=>{
+// // // // //     if(body.className === 'light'){
+// // // // //         body.className = 'dark';
+// // // // //     }
+// // // // //     else{
+// // // // //         body.className = 'light';
+// // // // //     }
+// // // // // });
+
+
+// // // // let button = document.querySelector('button');
+// // // // let body = document.querySelector('body');
+
+// // // // button.addEventListener('click',()=>{
+// // // //     body.classList.toggle('dark');
+// // // // });
+// // // let button = document.querySelector('button');
+// // // console.log(button);
+
+// // let firstbtn = document.querySelector('.firstbtn');
+// // firstbtn.onclick = ()=>{
+// //     console.log("onclick");
+// // }
+// // firstbtn.ondblclick = ()=>{
+// //     console.log('dubble click'); 
+// // }
+// let firstbtn = document.querySelector('.firstbtn');
+// let click = ()=>{
+//     console.log("1st");
+// }
+// firstbtn.addEventListener('click',click);
+// let rmdblclick = firstbtn.removeEventListener('click',click)
+
+// let dblclick = firstbtn.addEventListener('dblclick',()=>{
+//     console.log("doub");
+// });
